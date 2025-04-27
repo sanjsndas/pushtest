3dec72679cad8b162a82ce279972da7a333645e7
sanjsndas <jaintipu65658@gmail.com>
2025-04-27 08:27:28 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
-importScripts("https://cdn.pushalert.co/sw-81083.js");
+self.addEventListener('install', event => {
+  console.log('Service Worker installing.');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated.');
+});
+
+self.addEventListener('push', event => {
+  let data = {};
+  try {
+    data = event.data.json();
+  } catch (e) {
+    data = { title: 'New Notification', body: event.data.text() };
+  }
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: data.icon || '/icon.png'
+    })
+  );
+});
